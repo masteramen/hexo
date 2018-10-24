37b3c8435f581e7dc4e46447c1cef15fec7e713e
alex <2919798881@qq.com>
2018-10-24 18:42:32 +0800

    update

--- scripts/hexo-post-permalink-fix.js
+++ scripts/hexo-post-permalink-fix.js
@@ -4,29 +4,64 @@
  * Place this script in your project/scripts directory (project/scripts/hexo-post-permalink-fix.js)
  */
 
-const fs = require("hexo-fs");
-const path = require("path");
+'use strict';
+
+const fs = require('hexo-fs');
+const Promise = require('bluebird');
+
+const glob = require('glob')
+const path = require('path')
+
+function postsAssetGenerater(locals) {
+  const self = this;
+
+  function process(name) {
+    return glob
+    .sync('./source/_posts/**/**')
+    .filter(file => !/(.*)\.md$/.test(file) && fs.lstatSync(file).isFile()).map(file => {
+
+      const source = file;
+      let path = file.split('source/_posts/')[1];
+      const data = {
+        modified: false
+      };
+
+      data.data = () => fs.createReadStream(source);
+
+      return {
+        path,
+        data
+      };
+    });
+  }
+
+  return Promise.all([
+    process(),
+  ]).then(data => Array.prototype.concat.apply([], data));
+}
+hexo.extend.generator.register('postsAsset',postsAssetGenerater);
+//let time = new Date().getTime();
+
 
 // 处理 post 中图片
-hexo.extend.processor.register("posts/*path", function(file) {
+/*hexo.extend.processor.register("posts/*path", function(file) {
   if (file.type == "delete") return file;
   if (file.source.endsWith(".md") || file.source.endsWith(".markdown")) return;
   if (!/^_posts\//.test(file.path)) return;
 
-  /* console.log(file.path);
 
-  console.log(file.path.replace(/^_posts\//,''));
-  console.log(file.source);
 
-  hexo.route.set(file.path.replace(/^_posts\//,''), {
+ hexo.route.set(file.path.replace(/^_posts\//,''), {
     data: function(){
-      return fs.createReadStream(file.source)
+      return () => fs.createReadStream(file.source); 
     },
     modified: false
 });
-*/
 
-  hexo.extend.generator.register("test", function(locals) {
+
+  if(false)hexo.extend.generator.register((time++)+"a", function(locals) {
+    console.log('test...')
+    console.log(file.path.replace(/^_posts\//, ""))
     return {
       path: file.path.replace(/^_posts\//, ""),
       data: function() {
@@ -34,7 +69,7 @@ hexo.extend.processor.register("posts/*path", function(file) {
       }
     };
   });
-});
+});*/
 
 hexo.extend.filter.register(
   "post_permalink",
@@ -64,28 +99,24 @@ hexo.extend.filter.register(
 
 //将相对地址转为绝对地址
 hexo.extend.filter.register("before_post_render", function(data) {
-  const config = this.config.easy_images;
   var dir_post = path.join(this.source_dir, data.source);
-  var post_id = path.basename(data.source, ".md");
-  var dir_images = path.join(this.source_dir, post_id);
   var pattern = /!\[(.*?)\]\((.*?)\)/g;
-
   data.content = data.content.replace(pattern, (match, alt, src) => {
-    if (
-      path.dirname(src) != path.relative(path.dirname(dir_post), dir_images)
-    ) {
-      return match;
-    }
 
     let path_img = path.resolve(dir_post, "..", src);
-    let src_new = path_img.replace(this.source_dir, "/");
-
-    if (config && config.cdn_prefix && src_new[0] == "/") {
-      src_new = config.cdn_prefix + src_new;
-    }
+    let src_new = path_img.replace(this.source_dir+'_posts', "").replace(/\\/g,'/');
 
     return `![${alt}](${src_new})`;
   });
 
   return data;
 });
+
+
+//将相对地址转为绝对地址
+hexo.extend.filter.register("before_post_render", function(data) {
+
+  data.env = this.env.env;
+
+  return data;
+},15);
